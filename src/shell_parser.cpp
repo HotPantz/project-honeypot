ad54383e8169ee2e7b928ac4cee910aa15812504
hotpantz <selyankablia@gmail.com>
2024-11-12 13:03:37 +0100

    Readme updated, code commented and function explained, added .gitignore

--- src/shell_parser.cpp
+++ src/shell_parser.cpp
@@ -5,10 +5,28 @@
 
 extern int run_bg;
 
+// Function to create a new node with the given token
+/**
+ * @brief Creates a new node with the given token.
+ *
+ * This function allocates memory for a new node and initializes it with the provided token.
+ *
+ * @param token The token to be stored in the new node.
+ * @return A pointer to the newly created node.
+ */
 Node* createNode(const std::string& token) {
     return new Node(token);
 }
 
+// Function to add a new node with the given token to the end of the list
+/**
+ * @brief Adds a new node with the given token to the end of the list.
+ *
+ * This function creates a new node with the provided token and appends it to the end of the linked list.
+ *
+ * @param head A reference to the head pointer of the linked list.
+ * @param token The token to be added to the list.
+ */
 void enqueue(Node*& head, const std::string& token) {
     Node* newNode = createNode(token);
     if (!head) {
@@ -22,6 +40,15 @@ void enqueue(Node*& head, const std::string& token) {
     }
 }
 
+// Function to remove and return the token from the head of the list
+/**
+ * @brief Removes and returns the token from the head of the list.
+ *
+ * This function removes the head node from the linked list and returns its token.
+ *
+ * @param head A reference to the head pointer of the linked list.
+ * @return The token from the removed head node.
+ */
 std::string pop(Node*& head) {
     if (!head) {
         return "";
@@ -33,6 +60,14 @@ std::string pop(Node*& head) {
     return token;
 }
 
+// Function to free all nodes in the list
+/**
+ * @brief Frees all nodes in the list.
+ *
+ * This function deallocates memory for all nodes in the linked list, effectively clearing the list.
+ *
+ * @param head A reference to the head pointer of the linked list.
+ */
 void freeList(Node*& head) {
     while (head) {
         Node* temp = head;
@@ -41,6 +76,14 @@ void freeList(Node*& head) {
     }
 }
 
+// Function to print all tokens in the list
+/**
+ * @brief Prints all tokens in the list.
+ *
+ * This function iterates through the linked list and prints each token to the standard output.
+ *
+ * @param head A pointer to the head of the linked list.
+ */
 void printList(Node* head) {
     Node* current = head;
     while (current) {
@@ -52,6 +95,16 @@ void printList(Node* head) {
     }
 }
 
+// Function to get the nth token from the list
+/**
+ * @brief Gets the nth token from the list.
+ *
+ * This function returns the token at the specified position in the linked list.
+ *
+ * @param head A pointer to the head of the linked list.
+ * @param n The position of the token to retrieve (0-based index).
+ * @return The token at the specified position, or an empty string if the position is out of bounds.
+ */
 std::string getNthToken(Node* head, int n) {
     Node* current = head;
     int i = 0;
@@ -65,6 +118,15 @@ std::string getNthToken(Node* head, int n) {
     return ""; // if n is greater than the number of tokens
 }
 
+// Function to remove leading spaces from a string
+/**
+ * @brief Removes leading spaces from a string.
+ *
+ * This function returns a new string with all leading whitespace characters removed from the input string.
+ *
+ * @param str The input string.
+ * @return A new string with leading spaces removed.
+ */
 std::string removeLeadingSpaces(const std::string& str) {
     size_t start = 0;
     while (start < str.size() && std::isspace(static_cast<unsigned char>(str[start]))) {
@@ -73,6 +135,16 @@ std::string removeLeadingSpaces(const std::string& str) {
     return str.substr(start);
 }
 
+// Function to tokenize the input string and store tokens in the list
+/**
+ * @brief Tokenizes the input string and stores tokens in the list.
+ *
+ * This function splits the input string into tokens based on spaces and stores each token in the linked list.
+ * It also sets the run_bg flag if the "&" token is encountered.
+ *
+ * @param istream The input string to be tokenized.
+ * @param head A reference to the head pointer of the linked list.
+ */
 void tokenize(const std::string& istream, Node*& head) {
     std::string input = removeLeadingSpaces(istream);
     size_t start = 0;
@@ -97,6 +169,16 @@ void tokenize(const std::string& istream, Node*& head) {
     }
 }
 
+// Function to tokenize the input string considering quoted strings and store tokens in the list
+/**
+ * @brief Tokenizes the input string considering quoted strings and stores tokens in the list.
+ *
+ * This function splits the input string into tokens, taking into account quoted strings, and stores each token in the linked list.
+ * It also sets the run_bg flag if the "&" token is encountered.
+ *
+ * @param istream The input string to be tokenized.
+ * @param head A reference to the head pointer of the linked list.
+ */
 void tokenize2(const std::string& istream, Node*& head) {
     std::string token;
     bool inQuotes = false;
