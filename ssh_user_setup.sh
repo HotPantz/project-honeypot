4c614d6f3a3167db7bb131ed795404cb10b28805
EuroV8 <yeboy134@gmail.com>
2025-02-05 22:11:12 +0100

    Add home directory setup for the user

--- ssh_user_setup.sh
+++ ssh_user_setup.sh
@@ -1,5 +1,3 @@
-sudo useradd -M honeypot_user -s ~/Documents/project-honeypot/shell-emu/bin/fshell
-
+sudo useradd -M honeypot_user -d ~/Documents/project-honeypot/home -s ~/Documents/project-honeypot/shell-emu/bin/fshell
 sudo passwd honeypot_user
-
 chmod +x ~/Documents/project-honeypot/shell-emu/bin/fshell
