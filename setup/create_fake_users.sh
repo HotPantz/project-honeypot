88b673f779433b0246f3cb54e5c6b0660ebfc7c8
HotPantz <118857477+HotPantz@users.noreply.github.com>
2025-02-23 19:11:08 +0100

    We give ervery permissions to froot on the home folders

--- setup/create_fake_users.sh
+++ setup/create_fake_users.sh
@@ -18,41 +18,31 @@ USERS=(
   "crypto-admin"
 )
 
-# Nom du groupe commun
-GROUP="crypto-group"
-
-# Créer le groupe commun
-if ! getent group "$GROUP" > /dev/null; then
-  sudo groupadd "$GROUP"
-  echo "Created group: $GROUP"
-else
-  echo "Group $GROUP already exists"
-fi
-
-# Créer les utilisateurs avec les options spécifiques et les ajouter au groupe
+# Créer les utilisateurs avec les options spécifiques
 for user in "${USERS[@]}"; do
   if id "$user" &>/dev/null; then
     echo "User $user already exists"
   else
     if [ "$user" = "bitcoin-miner" ] || [ "$user" = "ethereum-miner" ]; then
-      sudo useradd -s /usr/sbin/nologin -G "$GROUP" "$user"
+      sudo useradd -s /usr/sbin/nologin "$user"
     else
-      sudo useradd -m -s /usr/bin/fshell -G "$GROUP" "$user"
+      sudo useradd -m -s /usr/bin/fshell "$user"
     fi
     echo "Created user: $user"
   fi
 done
 
-# Ajouter l'utilisateur froot au groupe
-sudo usermod -aG "$GROUP" froot
-echo "Added froot to group: $GROUP"
+# Ajouter l'utilisateur froot
+if ! id "froot" &>/dev/null; then
+  sudo useradd -m -s /usr/bin/fshell froot
+  echo "Created user: froot"
+fi
 
-# Modifier les permissions des répertoires personnels pour permettre l'accès au groupe
+# Donner tous les droits à froot sur les répertoires personnels des utilisateurs factices
 for user in "${USERS[@]}"; do
   if [ "$user" != "bitcoin-miner" ] && [ "$user" != "ethereum-miner" ]; then
-    sudo chown -R "$user:$GROUP" "/home/$user"
-    sudo chmod -R 770 "/home/$user"
-    echo "Modified permissions for /home/$user"
+    sudo setfacl -m u:froot:rwx "/home/$user"
+    echo "Granted all permissions to froot on /home/$user"
   fi
 done
 
